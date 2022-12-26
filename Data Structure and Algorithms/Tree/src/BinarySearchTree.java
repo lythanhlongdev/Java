5f043e27eaf308cc4e91ea21c9e0be015cbf65b4
lythanhlongdev <lythanhlong.fmc@gmail.com>
2022-12-26 13:33:37 +0700

    Data Structure and Algorithms

---
+++ Data Structure and Algorithms/Tree/src/BinarySearchTree.java
@@ -0,0 +1,13 @@
+public class BinarySearchTree {
+    private  class  Node{
+        private int info;
+        private Node left,right;
+        public Node (int info){
+            this.info = info;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    private Node root;
+
+}
