c388bcf892458312f86c36a60693c534960780b7
lythanhlongdev <lythanhlong.fmc@gmail.com>
2022-12-27 19:03:23 +0700

    Tree

--- Data Structure and Algorithms/Tree/src/Test.java
+++ Data Structure and Algorithms/Tree/src/Test.java
@@ -1,5 +1,14 @@
+import com.sun.source.tree.Tree;
+import com.sun.source.tree.TreeVisitor;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       BinarySearchTree x = new BinarySearchTree();
+       x.add(50);
+       for (int i = 0 ; i <= 100; i++){
+          if (i == 50) continue;
+          x.add(i);
+       }
+       x.nodeLeftRight();
     }
 }
