1f9ca944ff5cd731c09c47a193260f8acfb7d120
lythanh <lythanhlongfmc@gmail>
2023-03-29 03:33:57 +0700

    update jdbc

--- Database/src/database/JDBCUtil.java
+++ Database/src/database/JDBCUtil.java
@@ -4,9 +4,5 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class JDBCUtil {
-    public static Connection gConnection(){
-        Connection c = null;
-
-        return c;
     }
-}
+
